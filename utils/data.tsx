cc20a8984b223b0e80338ab3d1e54472b9501b92
RohitQAIG <rohit@thequantum.ai>
2025-03-21 16:30:22 +0530

    updated

--- utils/data.tsx
+++ utils/data.tsx
@@ -1,13 +1,9 @@
 import { 
-  SiPowerbi, SiMicrosoftexcel, SiMicrosoftword, SiMysql, 
-  SiPython, SiReact
+  SiPython
 } from 'react-icons/si';
 import { GiCircuitry } from 'react-icons/gi';
-import { MdOutlineFactory, MdOutlineMonitorHeart } from 'react-icons/md';
-import { VscServerProcess } from 'react-icons/vsc';
-import { TbBrandVscode } from 'react-icons/tb';
-import { FiBarChart2, FiActivity, FiTool, FiUsers, FiShoppingBag, FiCpu, FiGithub, FiLinkedin, FiMail } from 'react-icons/fi';
-import { SkillCategory, Experience, Project, Education, SocialLink, NavItem } from '@/types';
+import { FiBarChart2, FiCpu, FiGithub, FiLinkedin, FiMail } from 'react-icons/fi';
+import { SkillCategory, Experience, SocialLink, NavItem } from '@/types';
 
 // Navigation items
 export const navItems: NavItem[] = [
@@ -23,14 +19,14 @@ export const skillCategories: SkillCategory[] = [
   {
     name: 'Programming',
     skills: [
-      // { name: 'SCADA and HMI Programming', proficiency: 95, icon: () => <MdOutlineFactory className="w-6 h-6" /> },
-      // { name: 'Hardware Description Languages', proficiency: 85, icon: () => <GiCircuitry className="w-6 h-6" /> },
-      // { name: 'System Verilog', proficiency: 80, icon: () => <GiCircuitry className="w-6 h-6" /> },
-      // { name: 'Verilog', proficiency: 85, icon: () => <GiCircuitry className="w-6 h-6" /> },
-      // { name: 'Embedded Simulation & Design', proficiency: 80, icon: () => <GiCircuitry className="w-6 h-6" /> },
-      // { name: 'MATLAB', proficiency: 75, icon: () => <SiMatlab className="w-6 h-6" /> },
-      // { name: 'MASM', proficiency: 70, icon: () => <FiCpu className="w-6 h-6" /> },
-      // { name: 'PyCharm', proficiency: 80, icon: () => <SiPython className="w-6 h-6" /> },
+      { name: 'SCADA and HMI Programming', proficiency: 95, icon: <MdOutlineFactory className="w-6 h-6" /> },
+      { name: 'Hardware Description Languages', proficiency: 85, icon: <GiCircuitry className="w-6 h-6" /> },
+      { name: 'System Verilog', proficiency: 80, icon: <GiCircuitry className="w-6 h-6" /> },
+      { name: 'Verilog', proficiency: 85, icon: <GiCircuitry className="w-6 h-6" /> },
+      { name: 'Embedded Simulation & Design', proficiency: 80, icon:  <GiCircuitry className="w-6 h-6" /> },
+      { name: 'MATLAB', proficiency: 75, icon: <img src="/matlab_icon.webp" alt="MATLAB" className="w-6 h-6" /> },
+      { name: 'MASM', proficiency: 70, icon: <FiCpu className="w-6 h-6" /> },
+      { name: 'PyCharm', proficiency: 80, icon:  <SiPython className="w-6 h-6" /> },
     ]
   }
 ];
@@ -46,6 +42,7 @@ export const experiences: Experience[] = [
       "Produces monthly reports using advanced Excel spreadsheet functions",
       "Creates various Excel documents to assist with pulling metrics data and presenting information to stakeholders."
     ],
+    icon: <FiBarChart2 />,
     iconBackground: "#0a192f"
   }
 ];
@@ -55,16 +52,16 @@ export const socialLinks: SocialLink[] = [
   {
     name: "LinkedIn",
     url: "https://linkedin.com/in/ramyashree-velivela",
-    // icon: () => <FiLinkedin />
+    icon: <FiLinkedin />
   },
   {
     name: "GitHub",
     url: "https://github.com/ramyashree-velivela",
-    // icon: () => <FiGithub />
+    icon: <FiGithub />
   },
   {
     name: "Email",
     url: "mailto:ramyashree.velivela@example.com",
-    // icon: () => <FiMail />
+    icon: <FiMail />
   }
 ];
